f379bebd7ebc5bda125154fa4e8ddfdec2777312
neuralProcessor314 <biologicalnpu314@gmail.com>
2026-06-28 01:10:50 +0900

    FEAT ? help

--- f4vp-cli.py
+++ f4vp-cli.py
@@ -1,7 +1,7 @@
 import subprocess
 import pandas as pd
 
-print("faded-4B-views-predictor CLI. Type /? or /help for help.")
+print("faded-4B-views-predictor CLI. Type ? <none_or_command> or help <none_or_command> for usage help")
 while True:
     cmd = list(input("f4vp-cli>").split()) # split the given command by blankspaces.
 
@@ -12,20 +12,45 @@ while True:
     elif cmd[0] == 'show':
         subprocess.run(["py", "visualize.py"])
 
-    elif cmd[0] == 'getcfg':
+    elif cmd[0] == 'get':
         configs = pd.read_csv("configs.csv", index_col=0) # index_col=0 makes the func read the first column as indexes.
         try: # check if an argument exists. if so then print that config entry.
             cfg_name = cmd[1]
             print(configs.loc[cfg_name, "value"]) # row <cfg_name>, column <value>
         except IndexError: # if no arguments exist, then return all configs.
             print(configs)
-    elif cmd[0] == 'setcfg':
+    elif cmd[0] == 'set':
         configs = pd.read_csv("configs.csv", index_col=0, dtype='str') # if dtype is unspecified it defaults to int64
-                                                                       # and shows some errors cuz cfg_val is str.
-        cfg_name = cmd[1]
+        cfg_name = cmd[1]                                              # and shows some errors cuz cfg_val is str.
         cfg_val = cmd[2]
         configs.loc[cfg_name, "value"] = cfg_val
         configs.to_csv("configs.csv")
+
+    elif cmd[0] == '?' or  cmd[0] == 'help':
+        try:
+            if cmd[1] == 'fetch':
+                print('fetch             : run fetch.py to fetch data from the sheet then write to data/raw_data.csv.')
+            elif cmd[1] == 'predict':
+                print('predict           : run predict.py to fit the function to the data.')
+            elif cmd[1] == 'show':
+                print('show              : run visualize.py to show a plot of the data and the preds.')
+            elif cmd[1] == 'get':
+                print('get <none_or_cfg> : show the value of a configuration entry. if unspecified show all.')
+            elif cmd[1] == 'set':
+                print('set <cfg> <value> : set the value of a configuration entry.')
+            elif cmd[1] == '?':
+                print('? <none_or_cmd>   : show help for a given command. if unspecified show all.')
+            elif cmd[1] == 'help':
+                print('help <none_or_cmd>: show help for a given command. if unspecified show all.')
+
+        except IndexError:
+            print('fetch             : run fetch.py to fetch data from the sheet then write to data/raw_data.csv.')
+            print('predict           : run predict.py to fit the function to the data.')
+            print('show              : run visualize.py to show a plot of the data and the preds.')
+            print('get <none_or_cfg> : show the value of a configuration entry. if unspecified show all.')
+            print('set <cfg> <value> : set the value of a configuration entry.')
+            print('? <none_or_cmd>   : show help for a given command. if unspecified show all.')
+            print('help <none_or_cmd>: identical to ?.')
 
     elif cmd[0] == 'exit' or cmd[0] == 'quit':
         break
--- predict.py
+++ predict.py
@@ -8,8 +8,8 @@ save_dir = "params/pred_params.csv"
 # DATA PREPROCESSING
 print("Loading and processing data.")
 raw_data, time_d, time_h, time_m, views = load_data()
-time_d -= 10
-time_h -= 18
+time_d -= 10 # this line
+time_h -= 18 # and this line calibrates the data so that it becomes the relative time to June 10, 2026 18:00.
 time_tot = to_mins(time_d, time_h, time_m)
 
 # FITTING AND SAVING
