5c577ae80a21e1c936703e1c17ebdd6fdd145d0b
neuralProcessor314 <biologicalnpu314@gmail.com>
2026-06-28 14:03:28 +0900

    FEAT CSV-based tooltips

--- f4vp-cli.py
+++ f4vp-cli.py
@@ -1,9 +1,10 @@
 import subprocess
 import pandas as pd
 
-print("faded-4B-views-predictor CLI. Type ? <none_or_command> or help <none_or_command> for usage help")
+print("faded-4B-views-predictor. Type ? <none_or_command> or help <none_or_command> for usage help.")
 while True:
     cmd = list(input("f4vp-cli>").split()) # split the given command by blankspaces.
+    if len(cmd) == 0: continue # to avoid errors when no command is given.
 
     if cmd[0] == 'fetch':
         subprocess.run(["py", "fetch.py"])
@@ -24,34 +25,18 @@ while True:
         cfg_name = cmd[1]                                              # and shows some errors cuz cfg_val is str.
         cfg_val = cmd[2]
         configs.loc[cfg_name, "value"] = cfg_val
-        print(configs.loc[cfg_name, "value"])p
+        print(configs.loc[cfg_name, "value"])
         configs.to_csv("configs.csv")
 
     elif cmd[0] == '?' or  cmd[0] == 'help':
-        try:
-            if cmd[1] == 'fetch':
-                print('fetch             : run fetch.py to fetch data from the sheet then write to data/raw_data.csv.')
-            elif cmd[1] == 'predict':
-                print('predict           : run predict.py to fit the function to the data.')
-            elif cmd[1] == 'show':
-                print('show              : run visualize.py to show a plot of the data and the preds.')
-            elif cmd[1] == 'get':
-                print('get <none_or_cfg> : show the value of a configuration entry. if unspecified show all.')
-            elif cmd[1] == 'set':
-                print('set <cfg> <value> : set the value of a configuration entry.')
-            elif cmd[1] == '?':
-                print('? <none_or_cmd>   : show help for a given command. if unspecified show all.')
-            elif cmd[1] == 'help':
-                print('help <none_or_cmd>: show help for a given command. if unspecified show all.')
+        usages = pd.read_csv("usages.csv", index_col=0) # load the usage helps file.
+        try: print(usages.loc[cmd[1],"."])
+        except IndexError: # if cmd[1] does not exist; i.e. when no additional arguments  provided.
+            print('')
+            for i in range(usages.shape[0]): print(usages.iloc[i,0])
+            print('')
+        except KeyError: print(cmd[1], "is not a valid command.") # if cmd[1] is not a valid index.
 
-        except IndexError:
-            print('fetch             : run fetch.py to fetch data from the sheet then write to data/raw_data.csv.')
-            print('predict           : run predict.py to fit the function to the data.')
-            print('show              : run visualize.py to show a plot of the data and the preds.')
-            print('get <none_or_cfg> : show the value of a configuration entry. if unspecified show all.')
-            print('set <cfg> <value> : set the value of a configuration entry.')
-            print('? <none_or_cmd>   : show help for a given command. if unspecified show all.')
-            print('help <none_or_cmd>: identical to ?.')
-
-    elif cmd[0] == 'exit' or cmd[0] == 'quit':
+    elif cmd[0] == 'exit' or cmd[0] == 'quit' or cmd[0] == 'bye' or cmd[0] == 'seeyou' or cmd[0] == 'seeya':
+        print("Bye!")
         break
--- predict.py
+++ predict.py
@@ -6,19 +6,19 @@ from funcs import pred_func, load_data, load_cfg, to_mins
 save_dir = "params/pred_params.csv"
 
 # DATA PREPROCESSING
-print("Loading and processing data")
+print("Loading and processing data.")
 raw_data, time_d, time_h, time_m, views = load_data()
 time_d -= 10 # this line
 time_h -= 18 # and this line calibrates the data so that it becomes the relative time to June 10, 2026 18:00.
 time_tot = to_mins(time_d, time_h, time_m)
 
 # LOADING CFGS
-print("Loading configurations")
+print("Loading configurations.")
 iters = int(load_cfg().loc["iters", "value"])
 if iters == 0: iters = 500
 
 # FITTING AND SAVING
-print("Fitting")
+print("Fitting.")
 params = pd.DataFrame(optimize.curve_fit(pred_func, time_tot, views, maxfev=iters)[0])
 params.to_csv(save_dir)
 print("Saved params at " + save_dir + '.')
